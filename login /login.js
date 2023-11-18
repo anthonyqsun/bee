2b6fb15ac61aefe95c6c6af49ec34079d84eac99
Vijay Ravi <vijjr@umich.edu>
2023-11-18 16:18:05 -0500

    added room and images to login

--- login /login.js
+++ login /login.js
@@ -2,14 +2,15 @@ function submitUsername() {
     if(localStorage.getItem("name")){
         alert(localStorage.getItem("name") + " is logged in");
         document.getElementById("username").value = "";
+        document.getElementById("room").value = "";
         return;
     }
     let name = document.getElementById("username").value;
     console.log(name);
     document.getElementById("username").value = "";
-    localStorage.setItem("name", name);
+    document.getElementById("room").value = "";
 
-    databasePush();
+    localStorage.setItem("name", name);
 
 }
 
