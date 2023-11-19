c3bd7183f17300271473113bf46e45dc9b1e5d27
t1an-xyz <62950302+t1an-xyz@users.noreply.github.com>
2023-11-18 20:16:37 -0500

    add modals for time up

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 //  GLOBAL VARIABLES
 var sessionActive = false;
 
+//  ELEMENTS
+var sessionOptionsModal;
+
 class Session {
-    constructor(duration) {
-        this.startTime = new Date();
+    constructor(duration, breakLength) {
+        this.startTime = new Date(Date.now());
         this.endTime = new Date(this.startTime.getTime() + duration * 60000);
         this.duration = duration * 60000;
+        this.interrupted = false;
+        this.studyLength = duration;
+        this.breakLength = breakLength;
+        this.onBreak = false;
     }
 
     end() {
         this.endTime = new Date(Date.now());
+        this.interrupted = true;
+    }
+
+    startBreak() {
+        this.onBreak = true;
+        this.startTime = new Date(Date.now());
+        this.endTime = new Date(this.startTime.getTime() + this.breakLength * 60000);
+        this.duration = this.breakLength * 60000;
+    }
+
+    endBreak() {
+        this.onBreak = false;
+        this.startTime = new Date(Date.now());
+        this.endTime = new Date(Date.now() + this.studyLength * 60000);
+        this.duration = this.studyLength * 60000;
     }
 }
 
@@ -45,22 +67,54 @@ var setTimer = function () {
 
         if (minutes < 0) {
             clearInterval(timer);
-            localStorage.clear("session");
+            var studyLength = session.studyLength;
+            var breakLength = session.breakLength;
             txt.innerText = "Start";
             document.title = "StudyHive";
             sessionActive = false;
             rect.setAttribute("y", RECT_START_Y.toString());
+            if (!session.interrupted) {
+                if (session.onBreak) {
+                    var myModal = new bootstrap.Modal(document.getElementById('break-over-modal'), { backdrop: 'static', keyboard: false });
+                    myModal.show();
+                }
+                else {
+                    var myModal = new bootstrap.Modal(document.getElementById('study-over-modal'), { backdrop: 'static', keyboard: false });
+                    myModal.show();
+                }
+            }
+            else {
+                localStorage.removeItem("session");
+            }
         }
     }, 1000);
 }
 
-function newSession(duration) {
-    var session = new Session(duration);
+function startBreak() {
+    var dict = JSON.parse(localStorage.getItem("session"));
+    var session = new Session(dict["studyLength"], dict["breakLength"]);
+    session.startBreak();
+    localStorage.setItem("session", JSON.stringify(session));
+    setTimer();
+}
+
+function endBreak() {
+    var dict = JSON.parse(localStorage.getItem("session"));
+    var session = new Session(dict["studyLength"], dict["breakLength"]);
+    session.endBreak();
+    localStorage.setItem("session", JSON.stringify(session));
+    setTimer();
+}
+
+function newSession(duration, breakField) {
+    var session = new Session(duration, breakField);
     localStorage.setItem("session", JSON.stringify(session));
     setTimer();
 }
 
 window.onload = function () {
+    timerOptionsModal = new bootstrap.Modal(document.getElementById('timer-options-modal'), {});
+
     var button = document.getElementById("start-btn");
     button.addEventListener("mouseenter", function () {
         if (sessionActive) {
@@ -82,24 +136,30 @@ window.onload = function () {
         if (sessionActive) {
             var session = JSON.parse(localStorage.getItem("session"));
             session["endTime"] = Date(Date.now());
+            session["interrupted"] = true;
             localStorage.setItem("session", JSON.stringify(session));
-            console.log(JSON.stringify(session));
             return;
         }
 
-        const myModal = new bootstrap.Modal(document.getElementById('timer-options-modal'), {});
-        myModal.show();
+        timerOptionsModal.show();
+    });
 
-        var start = document.getElementById("modal-start-btn");
-        start.addEventListener("click", function () {
-            var lengthField = document.getElementById("session-length");
-            if (lengthField.value == "" || lengthField.value < lengthField.getAttribute("min") || lengthField.value > lengthField.getAttribute("max")) {
-                lengthField.style.borderBottomColor = "red";
-                return;
-            }
-            newSession(lengthField.value);
-            myModal.hide();
-        });
+    var start = document.getElementById("modal-start-btn");
+    start.addEventListener("click", function () {
+        var lengthField = document.getElementById("session-length");
+        var breakField = document.getElementById("break-length");
+        var studyLength = parseInt(lengthField.value);
+        if (studyLength == "" || studyLength < parseInt(lengthField.getAttribute("min")) || studyLength > parseInt(lengthField.getAttribute("max"))) {
+            lengthField.style.borderBottomColor = "red";
+            return;
+        }
+
+        var breakLength = parseInt(breakField.value);
+        if (breakLength == "" || breakLength < parseInt(breakField.getAttribute("min")) || breakLength > parseInt(breakField.getAttribute("max"))) {
+            breakField.style.borderBottomColor = "red";
+            return;
+        }
+        newSession(lengthField.value, breakField.value);
     });
 
     var session = localStorage.getItem("session");
