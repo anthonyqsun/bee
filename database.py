0dc259825fbeabd323ff96158642189ede2ce23b
t1an-xyz <62950302+t1an-xyz@users.noreply.github.com>
2023-11-19 00:32:30 -0500

    fix bug with db instead of self

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 class Database:
     def __init__(self):
         DB_FILE = "db"
@@ -29,23 +30,22 @@ class Database:
         )
 
     def addBee(self, room, name, task=""):
-        if name in db.getAllBeeInfo(room):
+        if name in self.getAllBeeInfo(room):
             self.c.execute(f"UPDATE bees SET room={room}, task='' WHERE name='{name}'")
         else:
             self.c.execute(
                 f"INSERT INTO bees (name, honey, task, room) VALUES (?, ?, ?, ?)",
-                (name, 0, task, room)
+                (name, 0, task, room),
             )
         self.commit()
 
     def addTask(self, room, task, color):
         self.c.execute(
             f"INSERT INTO tasks (task, color, room) VALUES (?, ?, ?)",
-            (task, color, room)
+            (task, color, room),
         )
         self.commit()
 
-        
     def commit(self) -> None:
         self.db.commit()
 
@@ -68,45 +68,52 @@ class Database:
     def getAllBeeInfo(self, room) -> dict:
         self.c.execute(f"SELECT name, honey, task FROM bees WHERE room='{room}'")
         data = self.c.fetchall()
-    
+
         out = {}
 
-        for (name,honey, task) in data:
+        for name, honey, task in data:
             out[name] = {"honey": honey, "task": task}
 
         return out
-    
+
     def getBeeInfo(self, room, bee) -> dict:
-        self.c.execute(f"SELECT honey, task FROM bees WHERE room='{room}' AND name='{bee}'")
+        self.c.execute(
+            f"SELECT honey, task FROM bees WHERE room='{room}' AND name='{bee}'"
+        )
         data = self.c.fetchall()[0]
 
         return {"honey": data[0], "task": data[1]}
-    
+
     def incrementHoney(self, room, bee):
-        self.c.execute(f"UPDATE bees SET honey={self.getBeeInfo(room, bee)['honey']+1} WHERE room='{room}' AND name='{bee}'")
+        self.c.execute(
+            f"UPDATE bees SET honey={self.getBeeInfo(room, bee)['honey']+1} WHERE room='{room}' AND name='{bee}'"
+        )
         self.commit()
-    
+
     def setTask(self, room, bee, task):
-        self.c.execute(f"UPDATE bees SET task={task} WHERE room='{room}' AND name='{bee}'")
-    
+        self.c.execute(
+            f"UPDATE bees SET task={task} WHERE room='{room}' AND name='{bee}'"
+        )
+
     def close(self) -> None:
         self.db.close()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     db = Database()
     db.createBeeTable("WHERE 100")
     db.createTaskTable("WHERE 100")
     db.addTask("WHERE 100", "watch", "green")
     db.addTask("WHERE 100", "watchs", "green")
-    db.addBee("WHERE 100", "a",1, "cooking")
-    db.addBee("WHERE 100", "b",1, "swimming")
-    db.addBee("WHERE 100", "c",1, "eating")
-    db.addBee("WHERE 100", "d",1,"")
+    db.addBee("WHERE 100", "a", 1, "cooking")
+    db.addBee("WHERE 100", "b", 1, "swimming")
+    db.addBee("WHERE 100", "c", 1, "eating")
+    db.addBee("WHERE 100", "d", 1, "")
     db.addBee("WHERE 100", "e")
     print(db.getAllBeeInfo("WHERE 100"))
     db.incrementHoney("WHERE 100", "e")
     print(db.getAllBeeInfo("WHERE 100"))
 
     print(db.getTasks("WHERE 100"))
-    
+
     db.close()
